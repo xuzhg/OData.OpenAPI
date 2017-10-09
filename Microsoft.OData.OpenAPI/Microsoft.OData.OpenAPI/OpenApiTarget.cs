9dd28f20c1a301b58f845ab13fae8a266e326eaa
Sam <saxu@microsoft.com>
2017-10-09 16:44:38 -0700

    small change for OpenApiTarget

--- Microsoft.OData.OpenAPI/Microsoft.OData.OpenAPI/OpenApiTarget.cs
+++ Microsoft.OData.OpenAPI/Microsoft.OData.OpenAPI/OpenApiTarget.cs
@@ -1,5 +1,5 @@
 ﻿//---------------------------------------------------------------------
-// <copyright file="ODataOpenApiTarget.cs" company="Microsoft">
+// <copyright file="OpenApiTarget.cs" company="Microsoft">
 //      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
 // </copyright>
 //---------------------------------------------------------------------
